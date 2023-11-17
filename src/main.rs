906271eb6786c325861db15e51e4d0750c1626bc
sunhui <sunhui@finogeeks.com>
2023-11-17 15:44:04 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #[macro_use] extern crate rocket;
+
+use std::net::Ipv4Addr;
+use rocket::config::{Config};
+
 mod json;
+
+
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
+    let mut config = Config::release_default();
+    config.address = Ipv4Addr::new(0, 0, 0, 0).into();
+
+    rocket::custom(config)
         .attach(json::stage())
 }
